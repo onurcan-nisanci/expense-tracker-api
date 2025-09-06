f8aad90329de2bb1bc0b47e7cca3935f6ca8651f
Onur <onurcan_nisanci@hotmail.com>
2025-09-06 16:26:25 +0100

    Initial commit

---
+++ ExpenseTracker.Models/Expense.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTracker.Models;
+
+public class Expense
+{
+    public Guid Id { get; set; }
+    public decimal Amount { get; set; }
+    public string Category { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+}
